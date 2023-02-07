0c707b03f02ca4f2be98fb31695d0e0e7d2ffa93
VisualSource <boomishere_network@outlook.com>
2023-02-07 18:27:57 -0500

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex h-20 w-full items-center bg-slate-100 px-8">
       <div className="text-xs font-normal not-italic text-gray-700">
-        © 2022, Midwest Raptor Junkies
+        © 2022-2023, Midwest Raptor Junkies
       </div>
     </footer>
   );
