f531d03b6128ecff7e1b39b3683b8608c3d7960d
VisualSource <boomishere_network@outlook.com>
2023-06-28 18:13:56 -0400

    remove social login plugin

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,12 +1,10 @@
-import { plugins } from 'cypress-social-logins';
+
 import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
-export const plugin = async (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
+export const plugin = async (on: (type: string, actions: object) => void, config: unknown) => {
     on("task", {
-        GoogleSocialLogin: plugins.GoogleSocialLogin,
-        FacebookSocialLogin: plugins.FacebookSocialLogin,
         async createPost() {
 
         }
