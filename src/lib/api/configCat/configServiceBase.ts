0021caba08ead1f07366337b3676d525d6899644
VisualSource <boomishere_network@outlook.com>
2023-07-18 17:29:01 -0400

    fix bug in configcat api

--- src/lib/api/configCat/configServiceBase.ts
+++ src/lib/api/configCat/configServiceBase.ts
@@ -10,7 +10,7 @@ export type ConfigFetcher = {
 
 export default class ConfigServiceBase {
   private route = `https://cdn.configcat.com/configuration-files/${process.env.CONFIG_CAT_KEY}/config_v4.json`;
-  constructor(protected cache: Cache) {}
+  constructor(protected cache: Cache) { }
   protected async fetch<T extends string | boolean | number = boolean>(
     lastConfig: ProjectConfig<T> | null,
     config?: RequestInit,
@@ -27,49 +27,47 @@ export default class ConfigServiceBase {
       }
 
       const response = await fetch(this.route, { ...config, headers });
-      if (!response.ok) throw response;
 
-      if (response.status === 200) {
-        const etag = response.headers.get("etag");
-        if (!etag) throw new Error("Failed to get etag");
-
-        const data = (await response.json()) as ProjectConfigJSON<T>;
-
-        return {
-          timestamp: new Date().getTime(),
-          configJSON: data,
-          httpETag: etag,
-        };
+      switch (response.status) {
+        case 200: {
+          const etag = response.headers.get("etag");
+          if (!etag) throw new Error("Failed to get etag");
+          const data = await response.json() as ProjectConfigJSON<T>;
+          return {
+            timestamp: new Date().getTime(),
+            configJSON: data,
+            httpETag: etag,
+          };
+        }
+        case 304: {
+          const etag = response.headers.get("etag");
+          if (!etag) throw new Error("Failed to get etag");
+          if (!lastConfig) throw new Error("Previous config was not set.");
+          return {
+            configJSON: lastConfig.configJSON,
+            timestamp: new Date().getTime(),
+            httpETag: etag,
+          };
+        }
+        default: {
+          logger.error(
+            response,
+            `Failed to download feature flags & settings from ConfigCat. Status: ${response && response.status
+            } - ${response && response.statusText}`,
+          );
+          logger.info(
+            "Double-check your SDK Key on https://app.configcat.com/sdkkey",
+          );
+          return lastConfig;
+        }
       }
-
-      if (response.status === 304) {
-        const etag = response.headers.get("etag");
-        if (!etag) throw new Error("Failed to get etag");
-        if (!lastConfig) throw new Error("Previous config was not set.");
-        return {
-          configJSON: lastConfig.configJSON,
-          timestamp: new Date().getTime(),
-          httpETag: etag,
-        };
-      }
-
-      logger.error(
-        response,
-        `Failed to download feature flags & settings from ConfigCat. Status: ${
-          response && response.status
-        } - ${response && response.statusText}`,
-      );
-      logger.info(
-        "Double-check your SDK Key on https://app.configcat.com/sdkkey",
-      );
-      return lastConfig;
     } catch (error) {
       const message =
         error instanceof Response
           ? error.statusText
           : error instanceof Error
-          ? error.message
-          : "";
+            ? error.message
+            : "";
       logger.error(
         error,
         `2Failed to download feature flags & settings from ConfigCat. Status: ${message}`,
