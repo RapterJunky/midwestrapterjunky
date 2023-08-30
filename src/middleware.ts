4c515f705496a049aabdb1d470e376bfa1e7a9dd
VisualSource <collin_blosser@yahoo.com>
2023-08-30 15:16:34 -0400

    remove logging

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
 import flagEnabled from "@lib/config/flagEnabled";
 import { Flags } from "@lib/config/flags";
-import { logger } from "./lib/logger";
 
 export async function middleware(request: NextRequest) {
   //if (request.nextUrl.pathname.startsWith("/404"))
@@ -27,16 +26,6 @@ export async function middleware(request: NextRequest) {
 
   if (request.nextUrl.pathname.startsWith("/plugins")) {
     const token = request.nextUrl.searchParams.get("token");
-
-    logger.info(
-      {
-        search: Object.fromEntries(request.nextUrl.searchParams.entries()),
-        token: process.env.PLUGIN_TOKEN,
-        matches: process.env.PLUGIN_TOKEN === token,
-      },
-      "Search params",
-    );
-
     if (token !== process.env.PLUGIN_TOKEN)
       return NextResponse.redirect(new URL("/not-found", request.url), 301);
   }
