62834421c7a7f021eb77f0284bf4901fbbe2ce1a
VisualSource <collin_blosser@yahoo.com>
2023-10-03 15:09:34 -0400

    add description to sponsor

--- src/app/(analytics)/(main)/(scrollOnlyNavbar)/sponsors/page.tsx
+++ src/app/(analytics)/(main)/(scrollOnlyNavbar)/sponsors/page.tsx
@@ -2,14 +2,21 @@ import type { Metadata, ResolvingMetadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Card, CardContent, CardDescription, CardFooter } from "@/components/ui/card";
-import SponsorsQuery, { type SponsorsQueryResult, } from "@/gql/queries/sponsors";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import SponsorsQuery, {
+  type SponsorsQueryResult,
+} from "@/gql/queries/sponsors";
 import getPageQuery from "@/lib/services/GetPageQuery";
 import getSeoTags from "@/lib/helpers/getSeoTags";
 import { Button } from "@/components/ui/button";
 
 export async function generateMetadata(
-  { },
+  {},
   parent: ResolvingMetadata,
 ): Promise<Metadata> {
   try {
@@ -22,8 +29,8 @@ export async function generateMetadata(
     return getSeoTags({
       parent,
       metadata: {
-        title: "Error"
-      }
+        title: "Error",
+      },
     });
   }
 }
@@ -32,14 +39,17 @@ const Sponsors: React.FC = async () => {
   const { sponsor } = await getPageQuery<SponsorsQueryResult>(SponsorsQuery);
   return (
     <div className="mb-4 flex w-full flex-grow flex-col items-center gap-6 divide-y divide-zinc-300">
-      <h1 className="md:leading-14 my-4 text-3xl font-extrabold leading-9 tracking-tight text-zinc-900 sm:text-4xl sm:leading-10 md:text-5xl mt-28">
+      <h1 className="md:leading-14 my-4 mt-28 text-3xl font-extrabold leading-9 tracking-tight text-zinc-900 sm:text-4xl sm:leading-10 md:text-5xl">
         Our Sponsors
       </h1>
-      <section className="grid w-full max-w-7xl grid-cols-1 gap-8 px-4 pt-4 md:grid-cols-2 lg:grid-cols-3 md:px-0">
-        {sponsor.sponsors.map((value, i) => (
+      <section className="grid w-full max-w-7xl grid-cols-1 gap-8 px-4 pt-4 md:grid-cols-2 md:px-0 lg:grid-cols-3">
+        {sponsor.sponsors.map((value) => (
           <Link href={value.link ?? "/"} key={value.id}>
-            <Card key={value.id} className="rounded-sm shadow flex flex-col h-full">
-              <div className="flex h-48 w-full items-center justify-center bg-zinc-200 p-2 hover:bg-zinc-300 flex-shrink">
+            <Card
+              key={value.id}
+              className="flex h-full flex-col rounded-sm shadow"
+            >
+              <div className="flex h-48 w-full flex-shrink items-center justify-center bg-zinc-200 p-2 hover:bg-zinc-300">
                 <div className="relative h-3/4 w-3/4">
                   <Image
                     className="object-contain object-center"
@@ -50,16 +60,18 @@ const Sponsors: React.FC = async () => {
                   />
                 </div>
               </div>
-              {i % 2 === 0 && value.description ? (
+              {value.description ? (
                 <CardContent className="p-4 pt-3">
-                  <CardDescription>
-                    {value.description}
-                  </CardDescription>
+                  <CardDescription>{value.description}</CardDescription>
                 </CardContent>
               ) : null}
-              <CardFooter className="flex justify-end items-end p-4 pb-3 flex-grow">
-                <Button size="sm" variant="secondary">View more</Button>
-              </CardFooter>
+              {value.link ? (
+                <CardFooter className="flex flex-grow items-end justify-end p-4 pb-3">
+                  <Button size="sm" variant="secondary">
+                    View more
+                  </Button>
+                </CardFooter>
+              ) : null}
             </Card>
           </Link>
         ))}
