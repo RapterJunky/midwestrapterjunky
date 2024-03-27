aa2fe7689287339191c8c78ea3c9bfebfad99ce1
VisualSource <collin_blosser@yahoo.com>
2024-03-27 17:05:45 -0400

    drop old migrations

--- prisma/migrations/20221209163342_add_is_dirty_prop/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `isDirty` to the `Cache` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Cache` ADD COLUMN `isDirty` BOOLEAN NOT NULL;
--- prisma/migrations/20230705161212_square_customer_id/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `sqaureId` VARCHAR(191) NULL;
